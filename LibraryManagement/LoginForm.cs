7144c2202acef0ee7d59f9eb4a4a03e93ce27763
Тхай Ха Нгуен <10lyanguyenhueschool@gmail.com>
2023-10-10 23:33:40 +0300

    login form commit

--- LibraryManagement/LoginForm.cs
+++ LibraryManagement/LoginForm.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using Microsoft.VisualBasic.ApplicationServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LibraryManagement
 {
@@ -25,24 +27,40 @@ namespace LibraryManagement
 
         private void LoginBtn_Click(object sender, EventArgs e)
         {
-            if (EmailBox.Text == "user123" && PasswordBox.Text == "123456")
+            string email = EmailBox.Text;
+            string password = PasswordBox.Text;
+            bool authorized = false;
+            
+            SqlConnection conn = new SqlConnection(MyGlobals.connectionStr);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("select * from dbo.Manager",conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            for (int i = dt.Rows.Count - 1; i >= 0; i--)
             {
-                MenuForm f1 = new MenuForm();
-                f1.Show();
-                this.Hide();
+                DataRow dr = dt.Rows[i];
+                if (email == dr[6].ToString().Trim() && password  == dr[1].ToString().Trim())
+                {
+                    authorized = true;
+                    MenuForm f1 = new MenuForm();
+                    f1.Show();
+                    this.Hide();
+                    break;
+                }
             }
-            else
+            if (!authorized) 
             {
                 string message = "Your user name or password is incorrect. Please try again";
                 string cap = "Error Detected in Login";
                 DialogResult loginError = MessageBox.Show(message, cap);
             }
-
         }
     }
 }
 
 public static class MyGlobals
 {
-    public static string connectionStr = "D:\\PROJECTCODE\\CSHARP\\GITCLONE\\LIBRARYMANAGEMENT\\LIB-DB\\DATABASE2.MDF";
+    public static string connectionStr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\10lya\\source\\repos\\projekt\\LibraryManagement\\Lib-DB\\Database2.mdf;Integrated Security=True;Connect Timeout=30";
 }
